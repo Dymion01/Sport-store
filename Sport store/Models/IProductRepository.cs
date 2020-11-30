6449787b95c79f77c25ad2cc3a2ffec3c641a5f8
Dymion <damianskorupa4@gmail.com>
2020-11-30 15:16:52 +0100

    Add Product class and IProductRepository Interface

---
+++ Sport store/Models/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sport_store.Models
+{
+    interface IProductRepository
+    {
+        IQueryable<Product> Products { get; }
+    }
+}
