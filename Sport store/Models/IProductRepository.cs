384ee00023f1499416391cb12237c6dd27e98958
Dymion <damianskorupa4@gmail.com>
2020-11-30 16:05:38 +0100

    Add ProductController

--- Sport store/Models/IProductRepository.cs
+++ Sport store/Models/IProductRepository.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace Sport_store.Models
 {
-    interface IProductRepository
+     public interface IProductRepository
     {
         IQueryable<Product> Products { get; }
     }
