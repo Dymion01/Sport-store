13bbcbc917e28c851b10f5ca35a5da950c98020c
Dymion <damianskorupa4@gmail.com>
2020-11-30 15:30:57 +0100

    Add IFakeProductRepository class and Update Startup class

---
+++ Sport store/Models/FakeProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sport_store.Models
+{
+    class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product>  {
+
+            new Product { Name = "Piłka" , Price = 22},
+             new Product { Name = "Deska" , Price = 212},
+              new Product { Name = "Buty" , Price = 322},
+        }.AsQueryable<Product>();
+
+    }
+}
--- Sport store/Startup.cs
+++ Sport store/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Sport_store.Models;
 
 namespace Sport_store
 {
@@ -16,6 +17,7 @@ namespace Sport_store
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IProductRepository, FakeProductRepository>();
             services.AddMvc();
         }
 
