7eb57f04ec3d17b900b33b521edd8e2d2d53e32c
Dymion <damianskorupa4@gmail.com>
2020-11-30 17:16:24 +0100

    Add AppDbContext and EFProductRepository classes

---
+++ Sport store/Models/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Sport_store.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
---
+++ Sport store/Models/EFProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sport_store.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private readonly AppDbContext context;
+        public EFProductRepository(AppDbContext ctx)
+        {
+
+            context = ctx;
+        }
+        public IQueryable<Product> Products => context.Products;
+    }
+}
