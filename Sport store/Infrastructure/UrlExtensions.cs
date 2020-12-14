844d434005c6e1d9e1f96f8d3395b062347a444b
Dymion <damianskorupa4@gmail.com>
2020-12-14 21:22:23 +0100

    Add UrlExtentions.cs and add "add to cart" button to ProductSummary

---
+++ Sport store/Infrastructure/UrlExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Sport_store.Infrastructure
+{
+    public static class UrlExtensions
+    {
+        public static string PathAndQuery(this HttpRequest reguest) =>
+            reguest.QueryString.HasValue ? $"{reguest.Path}{reguest.QueryString}" : reguest.Path.ToString();
+    }
+}
