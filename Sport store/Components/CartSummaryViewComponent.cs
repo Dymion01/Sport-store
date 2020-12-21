527779148dc5f3a6acde7761bdefb96ac5cd22e9
Dymion <damianskorupa4@gmail.com>
2020-12-21 16:42:30 +0100

    Add RemoveFormCart button and add CartSummary  View component and button for it

---
+++ Sport store/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sport_store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sport_store.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+
+}
