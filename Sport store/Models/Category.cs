c8e8732f5f9397a9f025a628525aa22694020053
Dymion <damianskorupa4@gmail.com>
2020-12-31 15:45:53 +0100

    Add data valitation to product and category models and update Edit view

--- Sport store/Models/Category.cs
+++ Sport store/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace Sport_store.Models
     public class Category
     {
         public int CategoryId { get; set; }
+        [Required(ErrorMessage = "Proszę określić kategorię.")]
+        [Display(Name = "Kategoria")]
         public string Name { get; set; }
     }
 }
